f0715b17bb02b851b46c9ea18e3e9b5cb8f5a234
Dominika <dominikadmt@gmail.com>
2021-01-14 11:49:13 +0100

    create burger, ingredients, controls, order summary and styles for them

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from "react";
-import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
-import Layout from "./containers/Layout/Layout";
+import React, { Component } from 'react';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Layout from './containers/Layout/Layout';
 
 class App extends Component {
   render() {
     return (
       <Layout>
-        <BurgerBuilder/>
+        <BurgerBuilder />
       </Layout>
     );
   }
