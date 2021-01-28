c777ecfaf20ffd1004c8ef5972905e26b02074e7
Dominika <dominikadmt@gmail.com>
2021-01-28 19:49:45 +0100

    implement lazy loading

--- src/App.js
+++ src/App.js
@@ -1,42 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense } from 'react';
+import { Route, withRouter, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from './containers/Checkout/Checkout';
 import Layout from './hoc/Layout/Layout';
-import { Route, withRouter, Redirect, Switch } from 'react-router-dom';
-import Orders from './containers/Orders/Orders';
-import Auth from './containers/Auth/Auth';
-import Logout from './containers/Auth/Logout/Logout';
+import Spinner from './components/UI/Spinner/Spinner';
 import * as actions from './store/actions/index';
+const Auth = React.lazy(() => import('./containers/Auth/Auth'));
+const Checkout = React.lazy(() => import('./containers/Checkout/Checkout'));
+const Logout = React.lazy(() => import('./containers/Auth/Logout/Logout'));
+const Orders = React.lazy(() => import('./containers/Orders/Orders'));
 
 class App extends Component {
   componentDidMount() {
     this.props.onTryAutoSignup();
   }
   render() {
-    let routes = (
-      <>
-        <Switch>
-          <Route path='/auth' component={Auth} />
-          <Route path='/' exact component={BurgerBuilder} />
-          <Redirect to='/' />
-        </Switch>
-      </>
-    );
-
-    if (this.props.isAuthenticated) {
-      routes = (
+    return (
+      <Layout>
         <>
           <Route path='/' exact component={BurgerBuilder} />
-          <Route path='/checkout' component={Checkout} />
-          <Route path='/orders' component={Orders} />
-          <Route path='/auth' component={Auth} />
-          <Route path='/logout' component={Logout} />
+          <Suspense fallback={<Spinner />}>
+            <Route path='/auth' component={Auth} />
+            <Route path='/checkout' component={Checkout} />
+            <Route path='/logout' component={Logout} />
+            <Route path='/orders' component={Orders} />
+          </Suspense>
           <Redirect to='/' />
         </>
-      );
-    }
-    return <Layout>{routes}</Layout>;
+      </Layout>
+    );
   }
 }
 
